2c19ddf70d93d4d864a91d0fad96b3cedfa834ec
Yulya <yulya919@yandex.ru>
2021-05-06 18:23:59 +0300

    gulp

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('load', function () {
     const btn = document.querySelector('button');
 
     btn.addEventListener('click', function () {
@@ -6,32 +6,32 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
     let clocks = () => {
-        let minutes = document.body.querySelector(".minutes");
-        let seconds = document.body.querySelector(".seconds");    
-        let minutes_data = 01;
-        let seconds_data = 59;
-       
-        let timer = setInterval(function() {
+        let minutes = document.querySelector(".minutes"),
+            seconds = document.querySelector(".seconds"),
+            colon = document.querySelector(".colon"),
+            minutes_data = minutes.dataset.min,
+            seconds_data = seconds.dataset.sec;
+
+        let timer = setInterval(function () {
             minutes.innerHTML = minutes_data;
-            seconds.innerHTML = ' : ' + seconds_data-- ;
-       
-            if(seconds_data < 10) { 
-                seconds.innerHTML = '<span>0</span>' + seconds_data;
+            colon.innerHTML = ':';
+            seconds.innerHTML = seconds_data--;
+
+            if (seconds_data < 10) {
+                seconds.innerHTML = '0' + seconds_data;
             }
-            if(minutes_data < 10) {
-                minutes.innerHTML = '<span>0</span>' + minutes_data;
+            if (minutes_data < 10) {
+                minutes.innerHTML = '0' + minutes_data;
             }
-            if(seconds_data == 0) {
+            if (seconds_data == 0) {
                 seconds_data = 59;
                 minutes_data--;
-                if(minutes_data < 0) {
+                if (minutes_data < 0) {
                     clearInterval(timer);
                     window.location.reload();
                 }
-
             }
-        }, 1000);  
-       
-        }
-        clocks();
+        }, 1000);
+    }
+    clocks();
 });
